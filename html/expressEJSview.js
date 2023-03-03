3e713d38557f1edaf93d1c7d6edbb2f6b2f96379
Daniel de Jesús Rosales Mendoza <dannas.cornell@gmail.com>
2023-03-03 18:15:55 +0000

    Add maxAge and etag

--- html/expressEJSview.js
+++ html/expressEJSview.js
@@ -4,7 +4,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use('/public',express.static('assets')); //middleware
+app.use('/public',express.static('assets',{
+    etag: false, //etag desactivado
+    maxAge: '5h' //tiempo válido de caché
+})); //middleware
+
+//If-None-Match: W/"Etag"
 
 app.get('/', function(request, response){
     response.render('index');
