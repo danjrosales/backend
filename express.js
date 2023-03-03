1243f6eb1e81d5de46e0e1807f8f54cc4511e781
Daniel de Jesús Rosales Mendoza <dannas.cornell@gmail.com>
2023-03-03 15:57:46 +0000

    ✨ Server with express added

---
+++ express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(request, response){
+    response.send('Hello there from express server')
+});
+
+app.get('/hello_get', function(request, response){
+    response.send(`Hola ${request.query.name}`); 
+    //https://dannascornell-supreme-space-journey-wrr455vj9vc9jp7-6000.preview.app.github.dev/hello?name=Daniel%20de%20Jes%C3%BAs
+});
+
+app.listen(6000);
